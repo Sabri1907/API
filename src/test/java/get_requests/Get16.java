2a5f8713ef0b86e65e58ea49774be8171ebdef2b
Sabri Guvendi <guvendisabri@gmail.com>
2022-11-12 00:46:55 +0300

    12.11.2022/1

--- src/test/java/get_requests/Get16.java
+++ src/test/java/get_requests/Get16.java
@@ -1,6 +1,17 @@
 package get_requests;
 
-public class Get16 {
+import base_url.DummyRestApiBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
+
+public class Get16 extends DummyRestApiBaseUrl {
     /*
            URL: https://dummy.restapiexample.com/api/v1/employees
            HTTP Request Method: GET Request
@@ -14,14 +25,14 @@ public class Get16 {
                    vi) Total salary of all employees is 6,644,770
     */
 
-    /*
-    Given
+    /*  Gherkin Language
+    Given => On kosullar
         https://dummy.restapiexample.com/api/v1/employees
 
     When
         User sends Get request
 
-    Then
+    Then => Assertions
          i) Status code is 200
     And
        ii) There are 24 employees
@@ -34,5 +45,35 @@ public class Get16 {
     And
        vi) Total salary of all employees is 6,644,770
      */
+
+    @Test
+    public void get16(){
+        spec.pathParam("first","employees");
+
+        Response response=given().spec(spec).when().get("/{first}");
+        response.prettyPrint();
+
+        // NOT: Ayni body icinde birden fazla assertion yaptigimizda soft assertion olur. Hata aldigimizda diger
+        // assertion'lar calismaya devam eder.
+        response.then().assertThat().body("data.employees",hasSize(24),
+                "data.employee_name",hasItems("Tiger Nixon", "Garrett Winters"));
+        // body'deki key'lerden herhangi birinin sayisini hasSize() methoduyla aldik ve 24 tane olup olmadigini assert
+        // yaptik. Sadece data ile de olurdu.
+        // hasItems() ile employee_name'ler arasinda aranan isimlerin olup olmadigini assert yaptik.
+
+        List<Integer> ages=response.jsonPath().getList("data.employee_age"); // Response'daki en buyuk yasi bulmak
+        // icin yaslari json formatina cevirip getList() methoduyla list'e cevirip bir list'e atadik.
+        System.out.println("ages = " + ages);
+        Collections.sort(ages);
+        System.out.println("SortedAges = " + ages);
+        // NOT=> List mutable'dir assign yapsak da yapmasak da sort yaptiktan sonra (uzerine yapilan degisiklikler)
+        // uzerine kaydedilir. Memory'de yeni bir yer acmaz. String bunun tam tersidir.
+        System.out.println(ages.get(ages.size()-1)); //66=> En sondaki elemani dondurdu
+        assertEquals(66,(int)ages.get(ages.size()-1)); // Explicitly narrowing yaptik.
+        // list'den gelen data'yi object olarak algiladi o yuzden hata verdi biz de basina(int) yazarak
+        // Explicitly narrowing yaptik.
+
+
+    }
 
 }
